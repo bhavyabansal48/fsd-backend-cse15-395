b0064c5fc335366b8de6777e633d589396bdb11b
bhavya bansal <bhavyabansal12416@gmail.com>
2026-08-19 22:10:24 +0530

    Experiment 1

---
+++ Experiment1/28-7index.js
@@ -0,0 +1,25 @@
+// console.log("Hello World");// used to print any message in js 
+// console.log(  "The sum of two numbers 3 and 6 is : "+ ( 3+6));
+// console.error("This is an error message");// red color in proper program
+// console.warn("This is a warn message");// yellow color in proper program
+// console.info("This is a information message");// blue color in proper program
+// console.debug("This is a debug message");// gray color in proper program
+// console.log(process.platform);// used to print the platform of the system
+// console.log(global.Lnumber);
+// global.Lnumber = "51";
+// console.log(global.Lnumber);
+// process.on('exit',function(){
+//     console.log('good');
+// }
+
+// // )
+// const{EventEmitter} = require('events');
+// const eventEmitter = new EventEmitter();
+// eventEmitter.on('lunch' , () => {
+//     console.log('good')
+// })
+// eventEmitter.emit('lunch')
+// eventEmitter.emit('lunch')
+
+const shyam = require("./xyz");
+console.log("hello", shyam)
